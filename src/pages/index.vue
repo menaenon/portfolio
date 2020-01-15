3c7f0bd3c7806c8a98c978b5a4cf47a808ea3625
menaenon <kamereonsaiko@yahoo.co.jp>
2020-01-15 15:39:18 +0900

    add-header

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,17 @@
 <template>
-  <div></div>
+  <Header />
 </template>
 
 <script>
+import Header from '@components/Header'
+
 export default {
   head: {
     title: 'Welcome',
     meta: [{ hid: 'description', name: 'description', content: 'Welcome' }]
+  },
+  components: {
+    Header
   }
 }
 </script>
