3b7a8b85c37d81169cf5e5cff49ce148eb189902
menaenon <kamereonsaiko@yahoo.co.jp>
2020-01-15 15:46:35 +0900

    fix-header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,4 @@
-<template>
+<template functional>
   <nav class="flex items-center justify-between flex-wrap bg-orange-900 p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
       <span class="font-semibold text-xl tracking-tight">Menaenon Portfolio</span>
@@ -16,7 +16,7 @@
         <a href="#Skill" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
           Skill
         </a>
-        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+        <a href="#Contact" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
           Contact
         </a>
       </div>
