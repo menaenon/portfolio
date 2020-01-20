670ec3ab18ef313c49dcc355a4ed5c57317d8c1c
menaenon <kamereonsaiko@yahoo.co.jp>
2020-01-20 13:33:53 +0900

    fix-index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,23 @@
 <template>
-  <Header />
+  <div>
+    <Header />
+    <pre>
+      content
+      content
+    </pre>
+    <Bottom />
+  </div>
 </template>
 
 <script>
 import Header from '@components/Header'
+import Bottom from '@components/Bottom'
 
 export default {
   head: {
     title: 'Welcome',
     meta: [{ hid: 'description', name: 'description', content: 'Welcome' }]
   },
-  components: {
-    Header
-  }
+  components: { Header, Bottom }
 }
 </script>
