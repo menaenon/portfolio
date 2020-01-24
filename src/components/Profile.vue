33dbbcab450eb070b5e1d70e6c4787ab9570018b
menaenon <kamereonsaiko@yahoo.co.jp>
2020-01-24 09:42:41 +0900

    fix-image

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -6,7 +6,7 @@
     >
       <div class="p-4 md:p-12 text-center lg:text-left">
         <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center">
-        <img src="src/assets/images/profile.webp">
+        <img src="../../src/assets/images/profile.webp">
         </div>
         <h1 class="text-3xl font-bold pt-8 lg:pt-0">t_o_d</h1>
         <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-teal-500 opacity-25"></div>
@@ -31,9 +31,7 @@
       </div>
     </div>
     <div class="w-full lg:w-2/5">
-      <img
-        src="https://cdn.pixabay.com/photo/2013/07/13/13/45/tux-161464_1280.png"
-        class="rounded-none lg:rounded-lg hidden lg:block"
+      <img src="../../src/assets/images/profile.webp" class="rounded-none lg:rounded-lg hidden lg:block"
       />
     </div>
   </div>
