b9d26cd771e692049bb2058ea75b354a9b21ef68
menaenon <kamereonsaiko@yahoo.co.jp>
2020-01-24 09:26:11 +0900

    remove-image

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -5,10 +5,9 @@
       class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0"
     >
       <div class="p-4 md:p-12 text-center lg:text-left">
-        <div
-          class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"
-          style="background-image: url('https://cdn.pixabay.com/photo/2013/07/13/13/45/tux-161464_1280.png')"
-        ></div>
+        <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center">
+        <img src="src/assets/images/profile.webp">
+        </div>
         <h1 class="text-3xl font-bold pt-8 lg:pt-0">t_o_d</h1>
         <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-teal-500 opacity-25"></div>
         <p class="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
